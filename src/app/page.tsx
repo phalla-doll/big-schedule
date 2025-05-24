f76f9a91ce61c40177fa585a4dd1fccc78736593
phalla.doll <phalla.doll@gmail.com>
2025-05-24 10:11:58 +0700

    fix: correct margin alignment in main container of Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Schedule from "@/components/template/schedule";
 export default function Home() {
   return (
     <div className="container sm:mx-auto min-h-screen">
-      <main className="min-h-screen flex flex-col items-center justify-center mx-4 sm:mx-0 py-15 sm:py-20">
+      <main className="min-h-screen flex flex-col items-center justify-center mx-4 sm:mx-auto py-15 sm:py-20">
         <Schedule />
       </main>
     </div>
